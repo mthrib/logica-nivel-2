7a2f78c12cef854b0c208df4664717a4cd362a31
mthrib <matheuseyorrana@gmail.com>
2025-03-20 20:41:16 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.
+A = int("10")
+B = int("20")
+
+a1 = A + 10
+b1 = B - 10
+
+print("A =", a1,"|",  "B =", b1)
