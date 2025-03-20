ca3138c089ac088133b7f2e58c1f77df4ee7e040
mthrib <matheuseyorrana@gmail.com>
2025-03-20 20:59:24 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -29,3 +29,35 @@ def C():
         print("c1 e c2 não são iguais")
 
 A(), B(), C()
+
+
+
+#Corrigido
+A1 = (4/2) + (2/4)
+A2 = 4/2 + 2/4
+
+B1 = 4/ (1+1) + 2/4
+B2 = 4/2 + 2/4
+
+C1 = 4+2 * 2 - 4
+C2 = 4 + 2 * 2 - 4
+
+def A():
+    print(A1, A2)
+    if A1 == A2:
+        print("a1 e a2 São iguais")
+    else:
+        print("a1 e a2 não são iguais")
+
+def B():
+    print(B1, B2)
+    if B1 == B2:
+        print("b1 e b2 são iguais")
+    else:
+        print("b1 e b2 não são iguais")
+
+def C():
+    print(C1, C2)
+    if C1 == C2:
+        print("c1 e c2 são iguais")
+    else:
